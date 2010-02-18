663d0c785006c5a17787e197a4416cf3bccce1f2
Brandon R. Stoner <monokrome@monokro.me>
2010-02-18 20:48:13 +0000

    Added an example of using this library to make a very generic snow effect.

---
+++ example/example.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+	particle_element = document.createElement('div');
+	$(particle_element).addClass('snowflake');
+
+	$('body').particles({
+		'parent': 'div#header',
+		'base_elements': [particle_element],
+		'update_particle':
+		{
+			'top': 4,
+			'left': function(ps)
+			{
+				return parseInt((Math.random()*5)+1);
+			}
+		},
+		'init_particle':
+		{
+			'top': function()
+			{
+				return Math.random()*($(this).parent().height());
+			},
+			'left': function()
+			{
+				return Math.random()*($(this).parent().width());
+			},
+			'background': function()
+			{
+				return function() { return 'url(./images/snowflake_0' + String(parseInt(Math.random()*2)+1) + '.png)'; }
+			}
+		},
+		'count': 20,
+		'incrementor': $.particles.generic_incrementors.bound_by_parent
+	});
+});
+
